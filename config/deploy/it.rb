2a3449a360fda1a81b873ceb86083e6ea7dc3867
Kedarnag Mukanahallipatna <kedarnag@Kedarnags-MacBook-Pro.local>
2015-03-02 11:09:12 -0800

    [master] Changes to database.yml and adds new file to deploy folder.

--- config/deploy/it.rb
+++ config/deploy/it.rb
@@ -9,10 +9,10 @@ set :log_level, :debug
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-role :app, %w{deploy@54.187.215.99}
-role :web, %w{deploy@54.187.215.99}
-role :db, %w{deploy@54.187.215.99}
-server '54.187.215.99', roles: %w{:web, :app, :db}, user: 'deploy'
+role :app, %w{deploy@it.event-scheduling.qwinixtech.com}
+role :web, %w{deploy@it.event-scheduling.qwinixtech.com}
+role :db, %w{deploy@it.event-scheduling.qwinixtech.com}
+server 'it.event-scheduling.qwinixtech.com', roles: %w{:web, :app, :db}, user: 'deploy'
 
 set :ssh_options, {
    #verbose: :debug,
