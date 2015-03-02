f75397d0fb08f8d660f8ed06917cd9ceca0bd4e4
Kedarnag Mukanahallipatna <kedarnag@Kedarnags-MacBook-Pro.local>
2015-03-02 11:11:24 -0800

    [master] Changes to database.yml and adds new file to deploy folder.

--- config/environments/it.rb
+++ config/environments/it.rb
@@ -1,4 +1,4 @@
-DeploymentApp::Application.configure do
+EventScheduling::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
