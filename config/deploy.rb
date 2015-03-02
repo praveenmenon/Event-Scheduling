d5719335a4077c8323249b81913855401ae33040
Kedarnag Mukanahallipatna <kedarnag@Kedarnags-MacBook-Pro.local>
2015-03-02 13:24:58 -0800

    [master] updates deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,36 @@
-# config valid only for Capistrano 3.1
 set :application, 'Event-Scheduling'
 set :repo_url, 'https://github.com/praveenmenon/Event-Scheduling.git'
-
+set :scm, :git
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :format, :pretty
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :linked_files, %w{config/database.yml config/security.yml}
+set :default_env, { :path => "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.0.0-p247'
+#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :bundle_gemfile, proc { release_path.join('Gemfile') }
+set :bundle_dir, proc  { shared_path.join('bundle') }
+set :bundle_flags, '--deployment --quiet'
+set :bundle_without, %w{development test}.join(' ')
+set :bundle_binstubs, proc  { shared_path.join('bin') }
+set :bundle_roles, :all
+set :bundle_bins, %w(gem rake rails)
+set :ssh_options, {
+   config: false
+}
+
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
 
-set :bundle_gemfile, -> { release_path.join('Gemfile') }
-set :bundle_dir, -> { shared_path.join('bundle') }
-set :bundle_flags, '--deployment --quiet'
-set :bundle_without, %w{development test}.join(' ')
-set :bundle_binstubs, -> { shared_path.join('bin') }
-set :bundle_roles, :all
-set :bundle_bins, %w(gem rake rails)
-set :whenever_roles, :all
 
+before 'deploy:updated', 'deploy:copy_database_yml'
 
 namespace :deploy do
 
@@ -37,17 +47,14 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), :in => :sequence, :wait => 5 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
-      puts "RESTARTED SUCCESSFULLY"
     end
   end
 
-  after :publishing, :restart
-
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:web), :in => :groups, :limit => 3, :wait => 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
@@ -55,4 +62,8 @@ namespace :deploy do
     end
   end
 
+  after :finishing, 'deploy:cleanup'
+
 end
+
+
