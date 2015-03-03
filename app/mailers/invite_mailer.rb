98fe513afddfdd3aaaaa1122a9d864555379b64b
praveenmenon <pmenon@qwinixtech.com>
2015-03-03 18:53:25 +0530

    Removes binding.pry

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,7 +3,6 @@ class InviteMailer < ApplicationMailer
 	default :from => 'pmenon@qwinixtech.com'
 
 	def send_email(invitee,creator)
-		binding.pry
 		@invitee = invitee
 		@creator=creator
 		mail( :to => @invitee.email,
